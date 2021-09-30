5bcb9a787da0bb9d368230e690430fe993c479bd
LOH Xiao Binn <xbloh.2018@sis.smu.edu.sg>
2021-09-30 14:55:23 +0800

    Add get Product APIs
    get all and get by category, unfinished

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 #from controller.Category import CategoryResource
 from controller.UserController import LoginController,RegisterController,LogoutController
-
+from controller.ProductController import ProductController
 from controller.Hello import Hello
 
 
@@ -15,4 +15,6 @@ api.add_resource(Hello,'/Hi')
 api.add_resource(LoginController,'/login')
 api.add_resource(RegisterController,'/register')
 api.add_resource(LogoutController,'/logout')
+
+api.add_resource(ProductController,'/product')
 #api.add_resource(CategoryResource,'/Category')
