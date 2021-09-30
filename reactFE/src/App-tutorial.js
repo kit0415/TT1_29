dba10c8f2ff571c0e9e30820ba70d59bb7ca2666
Suresh <sureshchao@gmail.com>
2021-09-30 12:46:55 +0800

    Basic Login Screen
    On button press submit, handleLoginSubmit  will be called. Pass in login api function in to handleLoginSubmit()

---
+++ reactFE/src/App-tutorial.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+import AddTutorial from "./components/AddTutorial";
+import Tutorial from "./components/Tutorial";
+import TutorialsList from "./components/TutorialsList";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          bezKoder
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/adds"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div id="lal" className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+          <Route exact path="/adds" component={AddTutorial} />
+          <Route path="/tutorials/:id" component={Tutorial} />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
